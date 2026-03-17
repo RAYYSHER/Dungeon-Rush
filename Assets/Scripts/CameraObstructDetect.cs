796d8d2ebfbf896d9454c41d6c1c6b73da0a20b4
RAYYSHER <achiwameraysheru@gmail.com>
2026-03-17 19:18:21 +0700

    transparent walls
    able to see player through walls and obstacles

---
+++ Assets/Scripts/CameraObstructDetect.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraObstructDetect : MonoBehaviour
+{
+    private Player player;
+    public LayerMask obstacleMask;
+
+    private HashSet<FadableObject> currentFaded = new HashSet<FadableObject>();
+
+    void Awake()
+    {
+        player = FindFirstObjectByType<Player>();
+    }
+
+    void LateUpdate()
+    {
+        Vector3 direction = player.transform.position - transform.position;// camera -> player
+        float distance = direction.magnitude;
+
+        Ray ray = new Ray(transform.position, direction);
+        RaycastHit[] hits = Physics.RaycastAll(ray, distance, obstacleMask);
+
+        if(hits.Length != 0)
+        {
+            Debug.Log("Wall detected amount = " + hits.Length);
+        }
+
+        HashSet<FadableObject> newFaded = new HashSet<FadableObject>();
+
+        foreach (var hitObstable in hits)
+        {
+            FadableObject fadeObject = hitObstable.collider.GetComponentInParent<FadableObject>();
+
+            if(fadeObject != null)
+            {
+                fadeObject.FadeOut();
+                newFaded.Add(fadeObject);
+            }
+
+        }
+
+        foreach (var fadedObstable in currentFaded)
+        {
+            if(!newFaded.Contains(fadedObstable))
+                fadedObstable.FadeIn();
+        }
+
+        currentFaded = newFaded;
+    }
+}
---
+++ Assets/Scripts/FadableObject.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadableObject : MonoBehaviour
+{
+    public float fadeAlpha = 0.3f;
+    public float fadeSpeed = 10f;
+    private List<Material> materials = new List<Material>();
+    public float targetAlpha = 1f;
+
+    void Awake()
+    {
+        Renderer[] renderers = GetComponentsInChildren<Renderer>();
+
+        foreach (var r in renderers)
+        {
+            // per renderer
+            foreach (var mat in r.materials)
+            {
+                materials.Add(mat);
+            }
+        }
+    }
+
+    void Update()
+    {
+        foreach (var mat in materials)
+        {
+            Color c = mat.GetColor("_BaseColor");
+            c.a = Mathf.Lerp(c.a, targetAlpha, Time.deltaTime * fadeSpeed);
+            mat.SetColor("_BaseColor", c);
+        }
+    }
+
+    public void FadeIn()
+    {
+        targetAlpha = 1f;
+        Debug.Log("Fading IN");
+    }
+
+    public void FadeOut()
+    {
+        targetAlpha = fadeAlpha;
+        Debug.Log("fading out");
+    }    
+}
