144ee794839e99dae15dd6d9334eba1e58af5dd8
RAYYSHER <achiwameraysheru@gmail.com>
2026-03-24 23:06:45 +0700

    Floor Boss 03
    + attack animation

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -4,13 +4,16 @@ public class Weapon : MonoBehaviour
 {
     public int attackDamage;
     public float attackRange = 1f;
+    public float attackCooldown = 1.5f;
     private Player player;
     public Animator animator;
     private Transform enemyRoot;
+    public Timer attackTimer;
 
     void Awake()
     {
         animator = GetComponentInParent<Animator>();
+        attackTimer = new Timer(attackCooldown);
 
         var combat = GetComponentInParent<Combat>();
         if (combat != null)
@@ -33,12 +36,14 @@ public class Weapon : MonoBehaviour
     }
     void Update()
     {
+        attackTimer.Update(Time.deltaTime);
+
         float distance = Vector3.Distance(enemyRoot.position , player.transform.position);
         
-        Debug.Log($"[{enemyRoot.name}] enemyRoot.pos={enemyRoot.position} | player.pos={player.transform.position} | dist={distance}");
-
-        if (distance <= attackRange)
+        if (distance <= attackRange && !attackTimer.IsRunning())
         {
+            attackTimer.Start();
+
             if (animator != null)
             {
                 animator.SetTrigger("trAttack");    
